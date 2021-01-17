b94f512f3a96b890a18d5734ac6905541c5e737f
fullmoonlong <54879271+fullmoonlong@users.noreply.github.com>
2021-01-18 08:53:40 +0900

    EnemyAI
    Enemy Artificial Intelligence added.
    (WIP)

---
+++ PuzzleMemo_M/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    float aiTurnTimer, passedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SoloModeCardControl.instance.GetIsMyTurn() == false)
+        {
+            Debug.Log(passedTime);
+            passedTime += Time.deltaTime;
+            aiTurnTimer = Random.Range(2f, 5f);
+
+            if (passedTime >= aiTurnTimer)
+            {
+                AICardFlip();
+            }
+        }
+    }
+
+    public void AICardFlip()
+    {
+        AIClickCheck();
+        SoloModeCardControl.instance.CardMove();
+        SoloModeCardControl.instance.CardBoardMatch();
+
+        //플레이어의 턴으로 변경
+        SoloModeCardControl.instance.SetIsMyTurn(true);
+        //SoloModeManager.instance.turnCount++;
+    }
+
+    void AIClickCheck()
+    {
+        //hit.transform.GetComponent<EnemyAI>().OpenCard();
+
+        //AI단계별 구현
+        ///WIP
+        /*
+        int randomCardPhase1, randomCardPhase2, randomCardPhase3, randomCardPhase4;
+        int hitCheckPhase2 = 0, hitCheckPhase3 = 0, hitCheckPhase4 = 0;
+
+        if (GetComponent<SoloModeManager>().turnCount <= 3)
+        {
+            randomCardPhase1 = Random.Range(1, 24);
+
+            GameObject.FindWithTag("card" + randomCardPhase1);
+        }
+        else if(GetComponent<SoloModeManager>().turnCount <= 8 && GetComponent<SoloModeManager>().turnCount > 3)
+        {
+            randomCardPhase2 = Random.Range(7, 24);
+            for(int i = 7; i <= 24; i++)
+            {
+                if (GameObject.FindWithTag("card" + i).GetComponent<SoloModeCardControl>().ishitted == true)
+                {
+                    hitCheckPhase2++;
+                }
+                else return;
+            }
+            GameObject.FindWithTag("card" + randomCardPhase2);
+        }
+        else if(GetComponent<SoloModeManager>().turnCount <= 12)
+        {
+            randomCardPhase3 = Random.Range(12, 24);
+        }
+        */
+
+        int randomCard = Random.Range(1, 24);
+
+        GameObject.FindWithTag("card" + randomCard).GetComponent<SoloModeCardControl>().OpenCard();
+    }
+}
