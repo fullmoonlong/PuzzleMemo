f647d6fa411beb8772df5c1048fe5d7191b795e1
StarSeed-Kor <ahshrnak@gmail.com>
2021-03-19 16:56:58 +0900

    Logic_Update
    1. 카드 셔플 전체적으로 적용

--- PuzzleMemo_M/Assets/Scripts/DoubleModeManager.cs
+++ PuzzleMemo_M/Assets/Scripts/DoubleModeManager.cs
@@ -35,6 +35,8 @@ public class DoubleModeManager : MonoBehaviour
         {
             Animals[i] = 0;
         }
+
+        Shuffle();
     }
 
     // Update is called once per frame
@@ -72,5 +74,40 @@ public class DoubleModeManager : MonoBehaviour
     public void Exit()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    void Shuffle()
+    {
+        int cardCnt = DoubleModeCardControl.cardCnt;//일반 카드 수
+        int S_cardCnt = 0; // 특수 카드 수
+
+        //카드 위치 정하기
+        /*for (int i = 1; i <= cardCnt + S_cardCnt; i++)
+        {
+            GameObject card = GameObject.FindWithTag("card" + i);
+
+            //시작위치부터 순서대로 생성
+            card.transform.position = new Vector3(cardX + (((i - 1) % 6) * cardGapX),
+                cardY - (((i - 1) / 6) * (cardGapY)), card.transform.position.z);
+        }*/
+
+        //카드 위치 섞기
+        for (int i = 1; i <= cardCnt + S_cardCnt; i++)
+        {
+            Debug.Log(i);
+            GameObject card = GameObject.FindWithTag("card" + i);
+            GameObject other_card = GameObject.FindWithTag("card" + UnityEngine.Random.Range(1, cardCnt + S_cardCnt + 1));
+
+            //자기 위치 저장
+            Vector3 cardPos = card.transform.position;
+
+            //서로 위치 바꾸기
+            card.transform.position = other_card.transform.position;
+            other_card.transform.position = cardPos;
+
+            //카드위치 정보 넘기기
+            card.GetComponent<DoubleModeCardControl>().cardPos = card.transform.position;
+            other_card.GetComponent<DoubleModeCardControl>().cardPos = other_card.transform.position;
+        }
     }
 }
